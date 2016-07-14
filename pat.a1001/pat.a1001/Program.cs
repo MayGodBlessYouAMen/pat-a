20fa356a074c2ffdeeef6435d48a77a862b416c1
gargoyle <gargoyle_shadow@qq.com>
2016-07-14 11:11:00 +0900

    pat.a1001

---
+++ pat.a1001/pat.a1001/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pat.a1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process();
+        }
+
+        private static void Process()
+        {
+            string input = Console.ReadLine();
+            string[] inputlist = input.Split(' ');
+            int a = int.Parse(inputlist[0]);
+            int b = int.Parse(inputlist[1]);
+            int c = a + b;
+            Console.WriteLine(c.ToString("#,0"));
+        }
+    }
+}
